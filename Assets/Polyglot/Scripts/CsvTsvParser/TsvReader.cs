7c6e900c35513fa7b6fe81f2e8de6bffb21d4009
Johannes Deml <public@deml.io>
2019-12-23 12:05:43 +0100

    Change make readers static classes, since no instances are wanted for those classes

--- Assets/Polyglot/Scripts/CsvTsvParser/TsvReader.cs
+++ Assets/Polyglot/Scripts/CsvTsvParser/TsvReader.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Polyglot
 {
-    public class TsvReader
+    public static class TsvReader
     {
         /// <summary>
         /// Parses the TSV string.
